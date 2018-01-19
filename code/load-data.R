d22ad67d03efce534a83fb2838e68d8c973416c4
Eugeni <32644567+eugenividal@users.noreply.github.com>
2018-01-19 17:20:07 +0000

    Starting point

--- code/load-data.R
+++ code/load-data.R
@@ -1,12 +1,9 @@
 # access data ----
 # dir.create("data")
 # file.edit(".gitignore")
-f = list.files("D:/Modules/JUMN5000M Understanding Data and its Environment/Assessments", full.names = T, pattern = "csv")
-f
-file.copy(f, "data/")
-
-# load packages for the work ----
-library(tidyverse)
+# f = list.files("D:/Modules/JUMN5000M Understanding Data and its Environment/Assessments", full.names = T, pattern = "csv")
+# f
+# file.copy(f, "data/")
 
 # read-in the data with read_csv() ----
 stores = read_csv("data/stores.csv")
